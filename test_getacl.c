f0fcde26c6b26c7aa6f1a77cccc7adac2e73a4c1
investigatorchic <investigatorchic@gmail.com>
2014-08-30 20:34:56 -0400

    updated test

--- test_getacl.c
+++ test_getacl.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <errno.h>
 
 int
 main(int argc, char *argv[])
 {
 	int result = getacl(argv[1], atoi(argv[2]), atoi(argv[3]));
-	printf("%d\n", result);
+	if (result == -1) {
+		printf("%d\n", (int)errno);
+	}
+	else {
+		printf("%d\n", result);
+	}
 	return (0);
 }
